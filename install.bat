cbf12e99c4680e33f460ab5f06d57e435e45e57a
Benjamin Nylund <ben@nylund.us>
2022-05-09 13:18:27 -0500

    Initial commit

---
+++ install.bat
@@ -0,0 +1,18 @@
+@echo off
+IF NOT EXIST OpenJDK (
+    mkdir OpenJDK
+)
+cd ./OpenJDK
+curl -z OpenJDK17U-jdk_x64_windows_hotspot_17.0.3_7.msi -o OpenJDK17U-jdk_x64_windows_hotspot_17.0.3_7.msi -L https://github.com/adoptium/temurin17-binaries/releases/download/jdk-17.0.3+7/OpenJDK17U-jdk_x64_windows_hotspot_17.0.3_7.msi
+OpenJDK17U-jdk_x64_windows_hotspot_17.0.3_7.msi
+cd ..
+IF NOT EXIST BuildTools (
+    mkdir BuildTools
+)
+cd BuildTools
+curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+set /p Input=Enter the version: || set Input=latest
+java -jar BuildTools.jar --rev %Input%
+cd ..
+cp .\BuildTools\spigot-*.jar spigot.jar
+pause
---
+++ start.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+java -Xms1G -Xmx8G -XX:+UseG1GC -XX:+UnlockExperimentalVMOptions -XX:MaxGCPauseMillis=100 -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:G1NewSizePercent=50 -XX:G1MaxNewSizePercent=80 -XX:G1MixedGCLiveThresholdPercent=35 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled -Dusing.aikars.flags=mcflags.emc.gs -DIReallyKnowWhatIAmDoingISwear=true -jar spigot.jar nogui
+exit
