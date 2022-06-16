c9277bd193ee81d84504f8731d20e469177c6f72
BuildTools <bnylund19@gmail.com>
2022-06-16 11:44:35 -0500

    Added bash scripts

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt update
+sudo apt-get install -y openjdk-17-jdk
+mkdir -p BuildTools
+cd BuildTools
+curl -z BuildTools.jar -o BuildTools.jar https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
+echo Enter a game version \(1.18.2, latest, 1.19, etc\): 
+read version
+java -jar BuildTools.jar --rev $version
+cd ..
+cp ./BuildTools/spigot-*.jar spigot.jar
+echo Done!
